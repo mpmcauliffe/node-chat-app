cb22f0e9cb5a89ea7760c24145b51c1accdb7642
mpmcauliffe <mcauliffe99@gmail.com>
2018-10-04 05:53:23 -0400

    add new message and create message events

--- server/server.js
+++ server/server.js
@@ -13,6 +13,21 @@ const express    = require('express'),
 app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
+
+    // socket.emit('newEmail', {
+    //     from: 'culy@doingcocky',
+    //     text: 'AAAAAAAAAAAA',
+    // });
+
+    socket.emit('createMessage', {
+        from: 'Jimbo',
+        text: 'rawwrrrawwwwmmmmm!',
+    });
+
+    // socket.on('createEmail', (newEmail) => {
+    //     console.log('createEmail', newEmail);
+    // });
+
     socket.on('disconnect', () => {
         console.log('client disconnected');
     })
