88fc78b3c1d4409ca60c12cc65f5095284ec1d14
mpmcauliffe <mcauliffe99@gmail.com>
2018-10-05 03:04:08 -0400

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: 'Otis',
+        text: 'Welcom to Brooks Falls. rawrrwaannnarrrnnmm!',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Otis',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    })
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
         io.emit('newMessage', {
